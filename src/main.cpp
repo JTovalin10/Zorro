70ec17ca5ada5d4d2b4a4ab326a1bc04afb156a8
jherna3 <jherna3@uw.edu>
2026-03-12 14:38:16 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <string>
 
+
+bool is_input_shell_type(const std::string& input) noexcept {
+  switch (input) {
+    case "echo":  return true;
+    case "break": return true;
+    default: return false;
+  }
+}
+
+/**
+ * Helper function that completes all other operations that are not exit
+ *
+ * ARGS:
+ * user_input - string that contains the user input
+ */
 void complete_operation(const std::string& user_input) noexcept {
   // check if is echo
-  if (user_input.size() >= 4 && user_input.substr(0, 4) == "echo") {
+  const bool geq_4 = user_input.size() >= 4;
+  if (geq_4 && user_input.substr(0, 4) == "echo") {
     std::cout << user_input.substr(5);
+  } else if (geq_4 && user.substr(0, 4) == "type") {
+    const bool is_type = is_input_shell_type(user.substr(5));
+    if (is_type) {
+      std::cout << user_input.substr(0, 4) << " is a shell builtin";
+    } else {
+      std::cout << user_input << ": not found";
+    }
   } else {
     std::cout << user_input << ": command not found";
   }
@@ -17,6 +40,7 @@ int main() {
   std::string user_input;
   while (true) {
     std::cout << "$ ";
+    // use get line to get the entire line
     std::getline(std::cin, user_input);
     if (user_input == "exit") {
       break;
